216799cb88e6a0ec3f8201a3b187cfdae9197e89
JoaKinG4 <joaquinmarcelo.mansilla@alumnos.ulagos.cl>
2025-06-23 19:27:57 -0400

    Se agrega

---
+++ Retos/RETO2.py
@@ -0,0 +1,16 @@
+
+#CREANDO EL DICCIONARIO
+inventario = dict(
+    Fruta1= 'Manzana',
+    Fruta2= 'Platano',
+    Fruta3= 'Platano',
+    Fruta4= 'Cereza'
+)
+
+#CREANDO LA TUPLA
+precios = (inventario['Fruta1'], 500), (inventario['Fruta2'], 700), (inventario['Fruta3'], 700), (inventario['Fruta4'], 200)
+
+
+#CREANDO LA LISTA
+tipos_frutas = (inventario), (precios)
+print(tipos_frutas)
---
+++ Retos/RetoN°3.py
@@ -0,0 +1,6 @@
+num = 500
+while num <= 500 and num > 100:
+    print(num)
+    num -=3
+
+
